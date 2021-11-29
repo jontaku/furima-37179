39e7f2fa0b36c9abd5abfb64e57d11537b8043bd
jontaku <kalemtaku7608@gmail.com>
2021-11-29 21:15:08 +0900

    userモデルのテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,7 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birth_date, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}
 
 end
