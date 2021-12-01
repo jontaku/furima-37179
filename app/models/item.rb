03cec2869a39328970ebd9f426fa4dd215823394
jontaku <kalemtaku7608@gmail.com>
2021-12-01 16:24:10 +0900

    ActiveHashのモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :shipping_charge, :prefecture, :shipping_day
+
+  
+  validates :genre_id, numericality: { other_than: 1 } 
 end
