f089bbdfc97ae3a818a1659479f7d1d80c708571
jontaku <kalemtaku7608@gmail.com>
2021-12-01 12:15:13 +0900

    画像保存のためのストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
