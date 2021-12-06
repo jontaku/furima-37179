f0ef3d4717fd18e61f659698b6d98575551a7887
jontaku <kalemtaku7608@gmail.com>
2021-12-06 12:48:00 +0900

    商品一覧を表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,13 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :shipping_charge, :prefecture, :shipping_day
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
-  validates :price, presence: true,
-                    numericality: { only_integer: true, less_than_or_equal_to: 9_999_999, greater_than_or_equal_to: 300 }
+  validates :price, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 9_999_999, greater_than_or_equal_to: 300 }
   with_options presence: true do
     validates :item_name
     validates :explanation
