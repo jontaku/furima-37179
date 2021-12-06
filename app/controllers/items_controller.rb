597a73ea2fcacf10bfc5e5c58ce76c4e9d9ceb2d
jontaku <kalemtaku7608@gmail.com>
2021-12-06 18:19:35 +0900

    コントローラーの記述をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  before_action :set_item, only:[:show, :edit, :update]
 
   def index
     @items = Item.all.order(id: :DESC)
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -46,5 +44,9 @@ class ItemsController < ApplicationController
   def correct_user
     item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == item.user.id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
